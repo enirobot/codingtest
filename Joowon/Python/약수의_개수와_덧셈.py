508b93f224a6c195ff9ccd76dee691d5eb33e22a
Joowon Moon <jsjs401@daum.net>
2022-09-02 10:42:05 +0900

    Update 약수의_개수와_덧셈.py

--- Joowon/Python/약수의_개수와_덧셈.py
+++ Joowon/Python/약수의_개수와_덧셈.py
@@ -1,28 +1,9 @@
-def caculate_divisor(num):
-    root_square = num**(1/2)
-    answer = 0
-    
-    if root_square == int(root_square):
-        root_square = int(root_square)
-        answer += 1
-    else:
-        root_square = int(root_square) + 1
-    
-    for i in range(1, root_square):
-        if num % i == 0:
-            answer += 2
-    
-    return answer
-
 def solution(left, right):
     answer = 0
-    
     for i in range(left, right+1):
-        cd = caculate_divisor(i)
-        if cd % 2 == 0:
-            answer += i
-        else:
-            answer -= i
-    
+        cd = i**(1/2)
+        
+        if cd == int(cd): answer -= i
+        else: answer += i
     
     return answer
