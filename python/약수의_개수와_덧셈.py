00e93c32e98bc240772d419d49d5b5d86b2e749e
enirobot <enirobot@naver.com>
2022-09-02 23:44:22 +0900

    약수의 개수와 덧셈

--- python/약수의_개수와_덧셈.py
+++ python/약수의_개수와_덧셈.py
@@ -1,7 +1,15 @@
+from math import isqrt, sqrt
 
-def solution(left: int, right: int):
-    answer = 0
-    return answer
+
+def solution(left: int, right: int) -> int:
+    total_number = 0
+    for number in range(left, right+1):
+        if isqrt(number) == sqrt(number):
+            total_number -= number
+        else:
+            total_number += number
+
+    return total_number
 
 
 if __name__ == "__main__":
