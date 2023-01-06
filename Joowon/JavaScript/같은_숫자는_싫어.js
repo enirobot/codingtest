bf5f55aa7e12005b0bbcef01fe4d10d54d6296de
Joowon Moon <jsjs401@daum.net>
2023-01-06 10:50:47 +0900

    Update 같은_숫자는_싫어.js

--- Joowon/JavaScript/같은_숫자는_싫어.js
+++ Joowon/JavaScript/같은_숫자는_싫어.js
@@ -1,19 +1,11 @@
 function solution(arr)
 {
     var answer = [];
-    const len = arr.length;
 
     answer = arr.filter((num, i) => {
-        let check;
-
         if (i === 0) return true;
-
-        if (arr[i-1] === num) {
-            return false;
-        } else {
-            return true;
-        }
-    })
+        return arr[i-1] !== num;
+    });
     
     return answer;
 }
